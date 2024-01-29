234e787edab02c43686ed4c0d800caa2ec7ddf67
muharremgursu <83711179+muharremgursu@users.noreply.github.com>
2024-01-29 13:02:45 +0000

    Database Eklendi

--- app.py
+++ app.py
@@ -1,42 +1,19 @@
 from flask import Flask, render_template, jsonify
- 
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-    
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'san Francisco, USA',
-    'salary': 'Rs. 15,00,000'
-  }
-]
+
 
 @app.route("/")
 def helloWorld():
-  return render_template('home.html', jobs=JOBS)
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company = 'Meri Baby')
 
 @app.route('/api/jobs')
 def list_jobs():
-  return  jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return  jsonify(jobs)
 
 
 
